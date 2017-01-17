3ab76b14d35364efc2dd0b603a3c3d21fd059873
Lektor role account <lektor@getdns.nlnetlabs.nl>
2017-01-17 16:03:50 +0100

    Update /dist dir

--- update_distdir.sh
+++ update_distdir.sh
@@ -12,8 +12,26 @@ done
 for f in content/releases/*/*.tar.gz
 do
 	distfile="${f#content/releases/*/}"
+	distdir="${f%$distfile}"
 	if [ ! -f "assets/dist/$distfile.sha256" ]
 	then
-		openssl sha256 "$f" > "assets/dist/$distfile.sha256"
+		(
+			cd "$distdir"
+			openssl sha256 "$distfile" > "../../../assets/dist/$distfile.sha256"
+		)
+	fi
+	if [ ! -f "assets/dist/$distfile.sha1" ]
+	then
+		(
+			cd "$distdir"
+			openssl sha1 "$distfile" > "../../../assets/dist/$distfile.sha1"
+		)
+	fi
+	if [ ! -f "assets/dist/$distfile.md5" ]
+	then
+		(
+			cd "$distdir"
+			openssl md5 "$distfile" > "../../../assets/dist/$distfile.md5"
+		)
 	fi
 done
